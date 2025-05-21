6b44c3fb6e6d5d95b028b2db35f7c0499e86e0eb
Lxg1c <teran_2005@mail.ru>
2025-05-21 13:14:46 +0300

    deleted comms

--- config.py
+++ config.py
@@ -3,9 +3,9 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 class Settings(BaseSettings):
     # Конфигурация Pydantic
     model_config = SettingsConfigDict(
-        env_file="./.env",  # Указываем, что переменные окружения загружаются из .env файла
-        env_file_encoding="utf-8",  # Указываем кодировку файла
-        case_sensitive=False,  # Не учитывать регистр
+        env_file="./.env",
+        env_file_encoding="utf-8",
+        case_sensitive=False,
     )
 
     BOT_TOKEN: str
@@ -15,4 +15,3 @@ class Settings(BaseSettings):
 
 # Создаем объект настроек
 settings = Settings()
-# print(settings.BOT_TOKEN)
